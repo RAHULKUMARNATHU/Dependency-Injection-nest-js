e6171962de32f15e35d176be61d88104863a97e2
Rahul Kumar <rahul.kumar@brainvire.com>
2022-12-01 12:51:23 +0530

    commit :: DI btwn Power and Disk Module

--- src/disk/disk.module.ts
+++ src/disk/disk.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PowerModule } from 'src/power/power.module';
 import { DiskService } from './disk.service';
 
 @Module({
+  imports:[PowerModule],
   providers: [DiskService]
 })
 export class DiskModule {}
--- src/disk/disk.service.ts
+++ src/disk/disk.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PowerService } from 'src/power/power.service';
 
 @Injectable()
-export class DiskService {}
+export class DiskService {
+
+constructor(private PowerService: PowerService){}
+
+getData(){
+    console.log('Drawing 20 watts of power from PowerService');
+    this.PowerService.supplyPower(20);
+    return 'data!';
+}
+}
